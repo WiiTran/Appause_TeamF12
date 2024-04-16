9a45d190900b1fdecf8f5146192d997a67f48e78
HuyTran <huyqtran@csus.edu>
2024-04-16 15:19:23 -0700

    Comple Login page, successfully connect with studentmainview,teachermainview,contenview, forgotpasswordview, and selectregistrationview, just need to design their UI

---
+++ Appause_TeamF12_HTr/StudentMainView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentMainView.swift
+//  Appause_TeamF12_HTr
+//
+//  Created by Huy Tran on 4/16/24.
+//
+
+import SwiftUI
+
+struct StudentMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentMainView()
+}
