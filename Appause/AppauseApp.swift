3d7e05fb73516b5d62b54c944d2afdeea581a3d6
Lsimoni1 <luke.simoni2001@gmail.com>
2024-04-21 20:54:41 -0700

    added plist file for Firebase and connected the SDK

--- Appause/AppauseApp.swift
+++ Appause/AppauseApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+var db = Firestore.firestore()
+let doc = db.collection("Users").document("StudentAemail@gmail.com")
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      print(doc.self)
+    return true
+  }
+}
 
 @main
 struct AppauseApp: App {
+    //register app delegate for firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
