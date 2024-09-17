35d9b486c042d94b3a8f7c02741be598362b4f13
Lsimoni1 <luke.simoni2001@gmail.com>
2024-09-17 12:40:50 -0700

    added app delegate and dependencies to connect to firebase

--- Appause/AppauseApp.swift
+++ Appause/AppauseApp.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 /*This is an environment variable used to determine which screen the back buttons on
   EmailCodeView, PWCodeVerificationView, and ResetPasswordView take the user to*/
@@ -13,13 +16,23 @@ class ViewSwitcher: ObservableObject{
     @Published var lastView = ""
 }
 
-
-
 @main
 struct Project190App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
