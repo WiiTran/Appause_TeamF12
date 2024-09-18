55f00a608f32a207b571dd4bb977612922b919ff
Rayanne Ohara <rohara@csus.edu>
2024-09-17 19:38:51 -0700

    new teacher main

---
+++ Appause/TeacherScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  TeacherScheduleView.swift
+//  Appause
+//
+//  Created by user on 9/17/24.
+//
+
+import SwiftUI
+
+struct TeacherScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeacherScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeacherScheduleView()
+    }
+}
