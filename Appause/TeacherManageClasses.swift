bfbe3dec3f1f567f5fa0e7f309faba355401c11d
WiiTran <huyqtran@csus.edu>
2024-11-12 18:16:30 -0800

    Delete Connect code in TeacherMain, Fetch TeacherData into Home teacher page, Commented Dark Mode, Adjust buttons for Teacher Main, Create ManageClasses to show all Classes. Fix UnlockRequestUITest.

---
+++ Appause/TeacherManageClasses.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherManageClasses.swift
+//  Appause
+//
+//  Created by Huy Tran on 11/12/24.
+//
+
+import SwiftUI
+
+struct TeacherManageClasses: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherManageClasses()
+}
